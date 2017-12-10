c63a4cae3940031eceab68a195b017ac3a9219f0
Dimitris Geromichalos <geromidg@gmail.com>
2017-12-11 00:43:07 +0100

    Implement the transformation of input pose from absolute to relative frame

--- tasks/Task.hpp
+++ tasks/Task.hpp
@@ -7,16 +7,30 @@ namespace gps_transformer {
 using Pose = base::samples::RigidBodyState;
 
 class Task : public TaskBase {
-  friend class TaskBase;
-
   public:
     explicit Task(std::string const& name = "gps_transformer::Task")
             : TaskBase(name) {}
 
   protected:
-    void inputPoseCallback(
+    void inputPoseTransformerCallback(
             const base::Time& timestamp,
-            const Pose& inputPose) override {}
+            const Pose& inputPose) override {
+        if (!initialPose_.hasValidPosition())
+            initialPose_ = inputPose;
+
+        Pose outputPose = inputPose;
+        outputPose.position = inputPose.position - initialPose_.position;
+
+        Eigen::Affine3d tf = outputPose.getTransform();
+        tf.prerotate(Eigen::AngleAxisd(_yawOffset.rvalue(),
+                Eigen::Vector3d::UnitZ()));
+        outputPose.setTransform(tf);
+
+        _outputPose.write(outputPose);
+    }
+
+  protected:
+    Pose initialPose_;
 };
 
 }  // namespace gps_transformer
