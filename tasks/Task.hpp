b65919a45ca25bb1bf6abd48b19baa770d6fea31
Dimitris Geromichalos <geromidg@gmail.com>
2017-12-10 22:15:00 +0100

    Create skeleton of component

--- tasks/Task.hpp
+++ tasks/Task.hpp
@@ -1,18 +1,23 @@
-#ifndef _GPS_TRANSFORMER_TASK_HPP_
-#define _GPS_TRANSFORMER_TASK_HPP_
+#pragma once
 
 #include "gps_transformer/TaskBase.hpp"
 
 namespace gps_transformer {
 
+using Pose = base::samples::RigidBodyState;
+
 class Task : public TaskBase {
   friend class TaskBase;
 
   public:
-    explicit Task(std::string const& name = "gps_transformer::Task");
+    explicit Task(std::string const& name = "gps_transformer::Task")
+            : TaskBase(name) {}
+
+  protected:
+    void inputPoseCallback(
+            const base::Time& timestamp,
+            const Pose& inputPose) override {}
 };
 
 }  // namespace gps_transformer
-
-#endif   // _GPS_TRANSFORMER_TASK_HPP_
 
